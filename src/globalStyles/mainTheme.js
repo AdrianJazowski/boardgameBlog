dccb1f3d83e1a45435d93dc8c934402158002ef0
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2021-04-06 21:45:22 +0200

    simplification of the theme

--- src/globalStyles/mainTheme.js
+++ src/globalStyles/mainTheme.js
@@ -1,33 +1,11 @@
 /** @format */
 
 export const mainTheme = {
-  //kolor czcionek #777;
-  // kolor specjalny czcionki #26cdc7
   primary: "#ffc033",
-  black: "rgb(26, 33, 47)",
-  secondary: "#26cdc7",
-  grey: "rgb(225, 225, 225)",
-  orange: "rgb(255, 114, 54)",
-  red: "rgb(204, 0, 0)",
-  yellow: "rgb(250, 197, 99)",
-  background: "rgb(255, 254, 253)",
-  backgroundMain: "rgb(255, 254, 253)",
-  borderRadius: "8px",
-  fontWeight: {
-    light: 300,
-    regular: 400,
-    bold: 700,
-  },
-  fontSize: {
-    xxs: "0.8rem",
-    xs: "1.2rem",
-    s: "1.6rem",
-    m: "2.2rem",
-    l: "3.6rem",
-    xl: "6.2rem",
-  },
+  black: "#000000",
+
   viewPorts: {
-    viewport4: "(min-width: 415px)",
-    viewport7: "(min-width: 1025px)",
+    viewport4: "(max-width: 415px)",
+    viewport7: "(max-width: 1025px)",
   },
 };
