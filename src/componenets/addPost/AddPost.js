8f05e5660060f06ea956a687f2bbff22b8c32204
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2021-04-11 18:59:38 +0200

    added set redirect after send post

--- src/componenets/addPost/AddPost.js
+++ src/componenets/addPost/AddPost.js
@@ -1,13 +1,15 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { addPostToFirestore } from "../../firebase/firestoreUtils";
 import { AddPostForm, AddPostWrapper, AddPostDiv } from "./AddPostStyles";
+import { Redirect } from "react-router-dom";
+import { routes } from "../../routes";
 
 const AddPost = () => {
   const selectedUser = useSelector(({ user }) => user);
-  console.log(selectedUser);
+  const [redirectUs, setRedirectUs] = useState(false);
 
   const handleAddPostForm = (e) => {
     e.preventDefault();
@@ -28,20 +30,25 @@ const AddPost = () => {
     addPostToFirestore(newPost);
 
     e.target.reset();
+
+    setRedirectUs(true);
   };
 
   return (
-    <AddPostWrapper>
-      <AddPostDiv>
-        <AddPostForm onSubmit={handleAddPostForm}>
-          <p>Tytuł posta</p>
-          <input name="postTitle" type="text" />
-          <p>Treść posta</p>
-          <textarea name="postDescription" type="text" />
-          <button type="submit">Wyślij</button>
-        </AddPostForm>
-      </AddPostDiv>
-    </AddPostWrapper>
+    <>
+      <AddPostWrapper>
+        <AddPostDiv>
+          <AddPostForm onSubmit={handleAddPostForm}>
+            <p>Tytuł posta</p>
+            <input name="postTitle" type="text" />
+            <p>Treść posta</p>
+            <textarea name="postDescription" type="text" />
+            <button type="submit">Wyślij</button>
+          </AddPostForm>
+        </AddPostDiv>
+      </AddPostWrapper>
+      {redirectUs ? <Redirect to={routes.home} /> : null}
+    </>
   );
 };
 
